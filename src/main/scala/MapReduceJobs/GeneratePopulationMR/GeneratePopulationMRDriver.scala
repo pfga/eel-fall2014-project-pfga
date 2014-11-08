ee2607d38e9f3e9c5e2d0f37e32f11d84bb4aeca
preethu thomas <preethu.thomas@ufl.edu>
2014-11-08 18:01:30 -0500

    Adding multiple output format code

--- src/main/scala/MapReduceJobs/GeneratePopulationMR/GeneratePopulationMRDriver.scala
+++ src/main/scala/MapReduceJobs/GeneratePopulationMR/GeneratePopulationMRDriver.scala
@@ -1,12 +1,13 @@
 package MapReduceJobs.GeneratePopulationMR
 
+import Main.PFGAConstants._
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.filecache.DistributedCache
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.io.{NullWritable => NW, Text => T}
 import org.apache.hadoop.mapreduce.Job
 import org.apache.hadoop.mapreduce.lib.input.{FileInputFormat, TextInputFormat}
-import org.apache.hadoop.mapreduce.lib.output.{FileOutputFormat, TextOutputFormat}
+import org.apache.hadoop.mapreduce.lib.output.{FileOutputFormat, MultipleOutputs, TextOutputFormat}
 
 /**
  * Created by preethu19th on 11/5/14.
@@ -15,6 +16,8 @@ object GeneratePopulationMRDriver {
   def run(conf: Configuration, ip: String, op: Path, fileName: String) = {
     DistributedCache.addCacheFile(new java.net.URI(fileName), conf)
     val job = new Job(conf)
+    MultipleOutputs.addNamedOutput(job, GENERATION,
+      classOf[TextOutputFormat[NW, T]], classOf[NW], classOf[T])
     job.setMapOutputKeyClass(classOf[NW])
     job.setMapOutputValueClass(classOf[T])
     job.setMapperClass(classOf[GeneratePopulationMapper])
@@ -23,7 +26,7 @@ object GeneratePopulationMRDriver {
     job.setInputFormatClass(classOf[TextInputFormat])
     job.setOutputFormatClass(classOf[TextOutputFormat[NW, T]])
     job.setJarByClass(GeneratePopulationMRDriver.getClass)
-    FileInputFormat.setInputPaths(job, ip)
+    FileInputFormat.setInputPaths(job, s"$ip/$GENERATION*")
     FileOutputFormat.setOutputPath(job, op)
     job.waitForCompletion(true)
   }

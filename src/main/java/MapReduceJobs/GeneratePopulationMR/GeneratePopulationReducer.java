7e1162902d7bbafd13be1c326f8e6a475062a1fa
sirmorya <ankit.sirmorya@ufl.edu>
2014-11-09 18:37:43 -0500

    Code for Reducer.

--- src/main/java/MapReduceJobs/GeneratePopulationMR/GeneratePopulationReducer.java
+++ src/main/java/MapReduceJobs/GeneratePopulationMR/GeneratePopulationReducer.java
@@ -4,6 +4,7 @@ import FuzzyTimeSeries.FuzzyIndividual;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
+import java.util.ArrayList;
 
 import java.io.IOException;
 //Reucer to perform GA
@@ -13,11 +14,11 @@ public class GeneratePopulationReducer extends
     public void reduce(NullWritable key,
                        Iterable<Text> values, Context context) throws
             IOException, InterruptedException {
+	ArrayList<FuzzyIndividual> individuals = new ArrayList<FuzzyIndividual>();
         for (Text value : values) {
             FuzzyIndividual fuzObj = new FuzzyIndividual();
             fuzObj.setChromosome(value.toString());
-            double fuzObjMse = fuzObj.mse();
-            int[] chromosome = fuzObj.chromosome();
+            indivuduals.add(fuzObj);            
             context.write(NullWritable.get(), new Text(fuzObj.toString()));
         }
     }

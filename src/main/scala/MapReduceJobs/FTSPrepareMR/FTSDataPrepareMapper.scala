332d143237dccd8ffcc27bb39302f328194994c7
sirmorya <ankit.sirmorya@ufl.edu>
2014-11-21 19:42:49 -0500

    Code Documentation.

--- src/main/scala/MapReduceJobs/FTSPrepareMR/FTSDataPrepareMapper.scala
+++ src/main/scala/MapReduceJobs/FTSPrepareMR/FTSDataPrepareMapper.scala
@@ -13,12 +13,12 @@ import org.apache.hadoop.mapreduce.Mapper
 class FTSDataPrepareMapper extends Mapper[LW, T, T, LW] with CommonMR {
   var mapRedFunc: ParseFunctions = _
 
-  //This acts as a setup function for the parser
+  //This acts as a setup function for the FTS
   override def setup(conT: Mapper[LW, T, T, LW]#Context) = {
     mapRedFunc = new ParseFunctions(conT.getConfiguration)
   }
 
-  //Processing of the parsing logic takes place in this mapper.
+  //Processing of the FTS logic takes place in this mapper.
   override def map(key: LW, value: T,
                    conT: Mapper[LW, T, T, LW]#Context) = {
     val (mapOpKey, mapOpVal) = mapRedFunc.mapFTSIp(value)
@@ -26,7 +26,7 @@ class FTSDataPrepareMapper extends Mapper[LW, T, T, LW] with CommonMR {
     conT.write(new T(mapOpKey), new LW(mapOpVal))
   }
 
-  //This method is used for changing the universe of discourse in every generation for the parser.
+  //This method is used for changing the universe of discourse in every generation for the FTS.
   override def cleanup(conT: Mapper[LW, T, T, LW]#Context) = {
     if (!firstRedOp) {
       conT.write(new T("min"), new LW(min))

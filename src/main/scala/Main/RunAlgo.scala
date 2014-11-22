373d5dccd9020ec3e3b77a22e71cad71aaf3cb77
sirmorya <ankit.sirmorya@ufl.edu>
2014-11-21 19:22:04 -0500

    Code documentation.

--- src/main/scala/Main/RunAlgo.scala
+++ src/main/scala/Main/RunAlgo.scala
@@ -11,36 +11,43 @@ import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.hadoop.util.GenericOptionsParser
 
 /**
+ * Driver Program
  * Created by preethu19th on 10/5/14.
  */
 object RunAlgo extends App {
+  //Instantiating the timing
   val startTime = System.nanoTime()
   val conf = new Configuration()
 
   val strArgs = new GenericOptionsParser(conf, args).getRemainingArgs
+  //Properties file to read from
   val configFileName = "parse-config.properties"
-  //strArgs(0)
+
+  //Setting Configurations
   ConfigReader.getConf(conf, configFileName)
   val ip = conf.get(ip_path)
   val opBasePath = conf.get(op_base_path)
   val fs = FileSystem.get(conf)
   fs.delete(new Path(opBasePath), true)
-
   val parseData = opBasePath + conf.get(parse_data_path)
   val ftsIp = opBasePath + conf.get(fts_ip_path)
   val gaOp = opBasePath + conf.get(ga_op_path)
   DataParserMRDriver.run(conf, ip, new Path(parseData))
+
+  //Instantiating the driver to run the data parser
   FTSDataPrepareMRDriver.run(conf, parseData, new Path(ftsIp))
+
+  //Printing the total time involved in parsing
   println((System.nanoTime - startTime) / NANOSECOND + " seconds")
 
-  //  val ftsIpFileName = "src/main/resources/input_fts_2.txt"
+  //Setting config for the Genetic Algorithm and FTS process.
   val ftsIpFileName = ftsIp + conf.get(reduce_part_filename)
-
   GeneratePopulationMRDriver.prepare(conf, gaOp, ftsIpFileName)
   val iterationCnt = conf.getInt(num_generation, 0)
   for (i <- 0 to iterationCnt) GeneratePopulationMRDriver.run(conf, gaOp, i)
   val stopTime = System.nanoTime()
 
+  //Printing the time and memory used for the prediction model.
   println((Runtime.getRuntime.totalMemory() - Runtime.getRuntime.freeMemory()) / 1024 / 1024)
   println((stopTime - startTime) / NANOSECOND)
 }

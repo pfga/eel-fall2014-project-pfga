c4acdb703cdd3a56f00abe0fcbeaef8c62d36365
sirmorya <ankit.sirmorya@ufl.edu>
2014-11-23 11:46:45 -0500

    Setting driver for the FTS operation.

--- src/main/scala/MapReduceJobs/FTSPrepareMR/FTSDataPrepareMRDriver.scala
+++ src/main/scala/MapReduceJobs/FTSPrepareMR/FTSDataPrepareMRDriver.scala
@@ -8,6 +8,7 @@ import org.apache.hadoop.mapreduce.lib.input.{FileInputFormat, TextInputFormat}
 import org.apache.hadoop.mapreduce.lib.output.{FileOutputFormat, TextOutputFormat}
 
 /**
+ * This is the driver class for the FTS process.
  * Created by preethu19th on 10/2/14.
  */
 object FTSDataPrepareMRDriver {
@@ -15,14 +16,18 @@ object FTSDataPrepareMRDriver {
     val job = new Job(conf)
     job.setMapOutputKeyClass(classOf[T])
     job.setMapOutputValueClass(classOf[LW])
+    //Setting the mapper and reducers
     job.setMapperClass(classOf[FTSDataPrepareMapper])
     job.setReducerClass(classOf[FTSDataPrepareReducer])
+    //Setting the number of reducers
     job.setNumReduceTasks(1)
     job.setInputFormatClass(classOf[TextInputFormat])
     job.setOutputFormatClass(classOf[TextOutputFormat[T, LW]])
     job.setJarByClass(FTSDataPrepareMRDriver.getClass)
+    //Setting the input and output paths
     FileInputFormat.setInputPaths(job, ip)
     FileOutputFormat.setOutputPath(job, op)
+    //Executing the job
     job.waitForCompletion(true)
   }
 }
